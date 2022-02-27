bba14814af092b03ab54510a19606aabbb8a5662
santos-pardos <66181446+santos-pardos@users.noreply.github.com>
2022-02-27 12:12:58 +0100

    Delete js directory

--- js/menu.js
+++
@@ -1,6 +0,0 @@
-let menu = document.getElementById('enlaces');
-let hamburguesa = document.getElementsByClassName('menu-header')[0];
-
-hamburguesa.addEventListener('click', function(){
-    menu.classList.toggle('extra-clase');
-})
